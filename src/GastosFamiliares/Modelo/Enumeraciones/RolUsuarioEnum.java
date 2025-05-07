92fd956131645d145bee79a0cb2edcbe3b81288f
JHOHN CARLOS ARRIETA ARRIETA <johncarlosarrietaarrietaa@gmail.com>
2025-05-07 13:01:29 -0500

    feature: creacion del proyecto, de los diagramas de casos de uso, del diagrama de clases, de la clase Entidad Usuario y de la la enumeracion RolUsuarioEnum

---
+++ src/GastosFamiliares/Modelo/Enumeraciones/RolUsuarioEnum.java
@@ -0,0 +1,11 @@
+
+package GastosFamiliares.Modelo.Enumeraciones;
+
+/**
+ *
+ * @author John Carlos Arrieta Arrieta
+ */
+public enum RolUsuarioEnum {
+    ADMINISTRADOR,
+    USUARIO
+}
---
+++ src/GastosFamiliares/Principal.java
@@ -0,0 +1,12 @@
+package GastosFamiliares;
+
+/**
+ *
+ * @author John Carlos Arrieta Arrieta
+ */
+public class Principal {
+    public static void main(String parametros[]){
+        System.out.println("Prueba del proyecto");
+
+    }
+}
